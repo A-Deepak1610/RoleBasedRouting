d5bf0cdadcbec3a300605830473c14a821592212
A-Deepak1610 <deepaka.ad24@bitsathy.ac.in>
2025-02-18 10:16:35 +0530

    final

--- src/applayout/Applayout.jsx
+++ src/applayout/Applayout.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import {Routes, Route,Navigate } from "react-router-dom";
 import Login from '../pages/Login';
 import UserDetails from '../store/Store'
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import UserDetails from '../store/Store'
 import { useNavigate } from "react-router-dom";
 export default function Login() {
@@ -6,10 +6,10 @@ export default function Login() {
   const [username, setUsername] = React.useState("");
   const [invaild,setInvalid]=useState("")
   const navigate = useNavigate();
+  useEffect(()=>{alert("Email:anyemail\nUserid=student or admin or faculty")},[]);
   function handlesumbit(event){
     event.preventDefault()
     login(username)
-    console.log(user);
     if (username.toLowerCase().includes("student")) {
       navigate("/RoleBasedRouting/student");
     } else if (username.toLowerCase().includes("faculty")) {
@@ -40,7 +40,7 @@ export default function Login() {
           />
 
           <label className="text-gray-600 text-sm mb-1" htmlFor="username">
-            Username
+            Userid
           </label>
           <input
             className="w-full px-4 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 mb-3"
